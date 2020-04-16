82f846d32d3f67ab1e0502d32be9e59d309611bb
Albert <friendsnowy@gmail.com>
2020-04-17 03:14:45 +0500

    Separate requests

--- src/features/auth/api/index.js
+++ src/features/auth/api/index.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
-
 export const login = async (login, password) => {
   const response = await axios.get(
     `http://localhost:5000/api/v1/auth?login=${login}&password=${password}`,
@@ -9,10 +8,7 @@ export const login = async (login, password) => {
   return response.data;
 };
 
-
-// Временно вместо реги
-/*
-export const login = async (login, password) => {
+export const register = async (login, password) => {
   const response = await axios.post(
     'http://localhost:5000/api/v1/auth',
     {
@@ -22,4 +18,3 @@ export const login = async (login, password) => {
   );
   return response.data;
 };
-*/
